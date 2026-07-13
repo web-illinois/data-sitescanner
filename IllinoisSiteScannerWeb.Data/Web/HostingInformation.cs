8f04970247d3c6cc6d0b8fa6b05c8564c70fac41
Bryan Jonker <jonker@illinois.edu>
2026-07-13 11:36:51 -0500

    cPanel+ wildcard

--- IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
+++ IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
@@ -4,9 +4,6 @@
 
         private static Dictionary<string, string> HostIpAddress = new() {
             { "75.2.20.195", "cPanel for Mission Critical Sites" },
-            { "18.154.185.54", "cPanel for Mission Critical Sites" },
-            { "18.154.185.40", "cPanel for Mission Critical Sites" },
-            { "18.154.185.35", "cPanel for Mission Critical Sites" },
             { "18.220.149.166", "cPanel" },
             { "18.160.200.125", "PIE" },
             { "13.59.228.241", "PIE" }
@@ -33,6 +30,7 @@
             HostIpAddress.TryGetValue(ip, out var valueAddress) ? valueAddress :
             HostnameAddress.TryGetValue(hostname, out var valueHostname) ? valueHostname :
             HostnameCms.TryGetValue(cms, out var valueCms) ? valueCms :
+            ip.StartsWith("18.154.185.") ? "cPanel for Mission Critical Sites" :
             "Other";
 
         internal static string GetLink(string hostname) => HelpLinksForHosting.TryGetValue(hostname, out var link) ? link : "";
