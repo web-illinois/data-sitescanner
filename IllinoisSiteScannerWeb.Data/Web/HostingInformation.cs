10f8e7b49c70db6e14c84c488ce768830f78888c
Bryan Jonker <jonker@illinois.edu>
2026-07-13 11:26:13 -0500

    Update to cPanel +

--- IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
+++ IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
@@ -3,6 +3,7 @@
     internal static class HostingInformation {
 
         private static Dictionary<string, string> HostIpAddress = new() {
+            { "75.2.20.195", "cPanel for Mission Critical Sites" },
             { "18.220.149.166", "cPanel" },
             { "18.160.200.125", "PIE" },
             { "13.59.228.241", "PIE" }
@@ -18,6 +19,7 @@
         };
 
         private static Dictionary<string, string> HelpLinksForHosting = new() {
+            { "cPanel for Mission Critical Sites", "" },
             { "cPanel", "" },
             { "PIE", "" },
             { "Sitefinity", "" },
