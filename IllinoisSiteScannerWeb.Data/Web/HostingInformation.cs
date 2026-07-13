1a2a948d2092c45b216b6cdf4616a8bc4785ff1a
Bryan Jonker <jonker@illinois.edu>
2026-07-13 11:31:04 -0500

    cPanel+ again

--- IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
+++ IllinoisSiteScannerWeb.Data/Web/HostingInformation.cs
@@ -4,6 +4,9 @@
 
         private static Dictionary<string, string> HostIpAddress = new() {
             { "75.2.20.195", "cPanel for Mission Critical Sites" },
+            { "18.154.185.54", "cPanel for Mission Critical Sites" },
+            { "18.154.185.40", "cPanel for Mission Critical Sites" },
+            { "18.154.185.35", "cPanel for Mission Critical Sites" },
             { "18.220.149.166", "cPanel" },
             { "18.160.200.125", "PIE" },
             { "13.59.228.241", "PIE" }
