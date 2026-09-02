f2cfd71d3f2ebc74248a22eff52ff8c415b7d4d1
Bryan Jonker <jonker@illinois.edu>
2026-09-02 08:22:09 -0500

    Updating URL to use https

--- IllinoisSiteScannerWeb.Emergency/EmergencyChecker.cs
+++ IllinoisSiteScannerWeb.Emergency/EmergencyChecker.cs
@@ -4,8 +4,10 @@ namespace IllinoisSiteScannerWeb.Emergency {
 
     public static class EmergencyChecker {
 
+        private static string EmergencyUrl => "https://content.getrave.com/cap/uiuc/channel1";
+
         public static Alert Check() {
-            var node = XElement.Load("http://content.getrave.com/cap/uiuc/channel1");
+            var node = XElement.Load(EmergencyUrl);
             var info = node.Descendants().FirstOrDefault(n => n.Name.LocalName == "info");
             if (info == null) {
                 return new Alert();
